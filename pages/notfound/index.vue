b8d3bfb1fe60620de9d3f6fce643f7dc91e7b6e7
SocheatKhoeun <159861545+SocheatKhoeun@users.noreply.github.com>
2025-02-13 22:30:56 +0700

    Notfound

--- pages/notfound/index.vue
+++ pages/notfound/index.vue
@@ -2,8 +2,8 @@
   <v-container class="fill-height d-flex align-center justify-center">
     <v-row class="d-flex align-center justify-space-between">
       <!-- Image -->
-      <v-col cols="12" md="6" class="d-flex justify-center">
-        <v-img src="/logo/icon-no-bg.svg" max-width="400" contain></v-img>
+      <v-col cols="12" md="6" class="d-flex justify-center mt-15">
+        <v-img src="/images/3d-casual-life-chatgpt-robot-holding-loupe.png" max-width="400" contain></v-img>
       </v-col>
 
       <!-- Error Message -->
@@ -11,14 +11,14 @@
         <v-chip color="blue lighten-4" class="text-caption font-weight-bold mb-3">
           Page Not Found
         </v-chip>
-        <h1 class="text-h4 font-weight-bold primary--text">OhNo!Error404</h1>
+        <h1 class="text-h4 font-weight-bold text-primary">OhNo!Error404</h1>
         <p class="text-body-1">
           Oops! The page you're looking for doesn't exist.
         </p>
         <p class="text-body-2">
           It might have been moved, deleted, or the URL could be incorrect.
         </p>
-        <v-btn color="#4667E5" to="/">Home Page <NuxtPage /></v-btn>
+        <v-btn color="#4667E5" to="/" class="mt-3" style="border-radius: 20px;">Home Page <NuxtPage /></v-btn>
       </v-col>
     </v-row>
   </v-container>
@@ -30,8 +30,3 @@ export default {
 }
 </script>
 
-<style scoped>
-.primary--text {
-  color: #4667E5;
-}
-</style>
