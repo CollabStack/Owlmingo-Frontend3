6c64ea957dc507f27312a640eb56c2196db37d00
Uch-Mengly <mengly_uch@student.tuxglobalinstitute.edu.kh>
2025-02-14 22:46:41 +0700

    10

--- components/auth/TelegramLogin.vue
+++ components/auth/TelegramLogin.vue
@@ -28,31 +28,20 @@ window.onTelegramAuth = (user) => {
 
 <template>
     <div>
-        <div id="telegram-login"></div>
         <button class="custom-telegram-button" @click="openTelegramAuth">
             <img src="/icons/icons8-telegram-48.svg" alt="Telegram Login" />
+            Login with Telegram
         </button>
     </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { onMounted } from "vue";
 
 const botUsername = "owlmingo_bot"; // Replace with your bot's username
-const scriptLoaded = ref(false);
 
 const openTelegramAuth = () => {
-    if (!scriptLoaded.value) {
-        const script = document.createElement("script");
-        script.src = "https://telegram.org/js/telegram-widget.js?22";
-        script.setAttribute("data-telegram-login", botUsername);
-        script.setAttribute("data-size", "large");
-        script.setAttribute("data-radius", "20");
-        script.setAttribute("data-onauth", "onTelegramAuth(user)");
-        script.setAttribute("data-request-access", "write");
-        document.getElementById("telegram-login").appendChild(script);
-        scriptLoaded.value = true;
-    }
+    window.Telegram.Login.auth({ bot_id: botUsername }, onTelegramAuth);
 };
 
 window.onTelegramAuth = (user) => {
