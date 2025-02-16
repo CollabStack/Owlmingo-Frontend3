47cf14709c610c1a7a9c85c21819bd393950a83a
Uch-Mengly <mengly_uch@student.tuxglobalinstitute.edu.kh>
2025-02-17 00:52:50 +0700

    Add initialization log for userAuth store in Nuxt plugin

--- plugins/initAuth.client.ts
+++ plugins/initAuth.client.ts
@@ -1,6 +1,7 @@
 import { userAuth } from '~/store/userAuth';
 
 export default defineNuxtPlugin(() => {
+    console.log('Initializing userAuth store');
     const authStore = userAuth();
     authStore.initialize();
 });
