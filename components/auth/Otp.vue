290509866fda425ad0d2e838fe02276c833ee69d
Slade <ken.ex350@gmail.com>
2025-02-18 17:53:20 +0700

    refactor: update Signupped component layout and styling for improved user experience

--- components/auth/Otp.vue
+++ components/auth/Otp.vue
@@ -1,201 +1,141 @@
+
+
+
 <template>
-    <div class="login-container">
-            <div class="left-side">
-                <h2 class="welcome-text">Verification</h2>
-                <p class="enter-code-text">Enter the verification code</p>
-                <div class="otp-input-container">
-                    <input type="text" class="otp-input" maxlength="1" />
-                    <input type="text" class="otp-input" maxlength="1" /> 
-                    <input type="text" class="otp-input" maxlength="1" />
-                    <input type="text" class="otp-input" maxlength="1" />
-                    <input type="text" class="otp-input" maxlength="1" />
-                    <input type="text" class="otp-input" maxlength="1" />
-                </div>
-                <div class="resend-container">
-                    <div class="empty-box"></div>
-                    <a href="#" class="resend-link">If you didn't receive a code, Resend</a>
-                </div>
-                <button class="send-button">Send</button>
-                <div class="rectangle">
-                    <div class="line">
-                        <span class="or-continue">or continue with</span>
-                    </div>
-                </div>
-                <div class="social-icons">
-                  <a href="#" class="icon-link">
-                    <img src="hC:\Owlmingo-Frontend3\public\icons\icons8-google-480.png" alt="Google" class="icons" />
-                  </a>
-                  <a href="#" class="icon-link">
-                    <img src="C:\Owlmingo-Frontend3\public\icons\icons8-github-480.png" alt="GitHub" class="icons"/>
-                  </a>
-                  <a href="#" class="icon-link">
-                    <img src="hC:\Owlmingo-Frontend3\public\icons\icons8-telegram-144.svg" alt="Telegram" class="icons"/>
-                  </a>
-                </div>
-                <div>
-                    <button class="sign-up">Don't have an account? Sign Up</button>
-                </div>
-            </div>
-      <div class="left-side" style="width: 50%; height: 100%;">
-        <img src="C:\Owlmingo-Frontend3\public\images\3d-business-man-working-at-laptop-with-robot-assistant.png" alt="" class="image" />
-        
-      </div>
-    </div>
-</template>
-
-<style>
-  .login-container {
-    margin-left: 150px;
-    display: flex;
-    border-radius: 20px;
-    overflow: hidden;
-    justify-items: space-between;
-  }
-
-  .left-side {
-    background-color: #fff;
-    padding: 40px;
-    display: flex;
-    flex-direction: column;
-    justify-items: center;
-    align-items: center;
-  }
-
-  .right-side {
-    display: flex;
-    background-color: #ffffff;
-    justify-content: center;
-  }
-
-  .image{
-    width: 400px;
-    height: cover;
-    margin-left: -250px;
-  }
-
-  .welcome-text {
-    width: 700px;
-    color: #00B0FF;
-    text-align-last: center;
-    font-size: 24px;
-    margin-bottom: 20px;
-  }
-
-  .enter-code-text {
-    width: 45%;
-    color: #b2b7f3;
-    text-align: left;
-    font-size: 13px;
-    margin-bottom: 20px;
-  }
-
-  .otp-input-container {
-    display: flex;
-    justify-content: space-between;
-    width: 45%;
-    margin-bottom: 20px;
-  }
-
-  .otp-input {
-    width: 40px;
-    height: 40px;
-    font-size: 20px;
-    text-align: center;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-  }
-
-  .resend-container {
-    display: flex;
-    width: 45%;
-    margin-bottom: 20px;
-    align-items: center;
-  }
-  .empty-box{
-    width: 25%;
-  }
-
-  .resend-link {
-
-    color: #00B0FF;
-    text-decoration: none;
-    font-size: 10px;
+    <v-container class="container fill-height mt-6" >
+      <v-row class="d-flex flex-row justify-center align-center">
+        <v-col class="d-flex flex-column justify-center align-center ml-16" style="align-items: center; margin-left: -20px;">
+          <h2  style="color: skyblue;">Welcome to Study</h2>
   
-  }
+          <v-form class="d-flex flex-column mt-5 " style="width: 350px;">
+            <h5 class=" align-left justify-start w-100" style="color: skyblue;">Welcome to Study</h5>
 
-  .send-button {
-    width: 45%;
-    background-color: #00B0FF;
-    color: #fff;
-    padding: 10px;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    margin-bottom: 20px;
-  }
+            <v-otp-input class="mb-2"></v-otp-input>
+            
+            <v-row class="justify-center w-100 mb-1">
+            <a href="#">if you didn't Recieve the code, Resend</a>
+          </v-row>
+            
+  
+            
+  
+          <v-container class="d-flex flex-column justify-center align-center" >
+            <div class="rectangle w-100">
+              <div class="line w-50">
+                <span class="or-continue">or continue with</span>
+              </div>
+            </div>
+            <div class="social-icons">
+              <a href="#" class="icon-link">
+                <img src="C:\Owlmingo-Frontend3\public\icons\icons8-google-480.png" alt="Google" class="icons" />
+              </a>
+              <a href="#" class="icon-link">
+                <img src="C:\Owlmingo-Frontend3\public\icons\icons8-github-480.png" alt="GitHub" class="icons" />
+              </a>
+              <a href="#" class="icon-link">
+                <img src="C:\Owlmingo-Frontend3\public\icons\icons8-telegram-144.svg" alt="Telegram" class="icons" />
+              </a>
+            </div>
 
+            <div class="sign-up" style="border: 1px solid black; width: 100%;">
+              <span>Don't have an account?</span>
+              <a href="#" class="sign-up-link">Sign Up</a>
+            </div>
+          </v-container>
+  
+          </v-form>
+        </v-col>
+  
+        <div class="right-side w-50 mr-n9">
+          <img 
+            src="C:\Owlmingo-Frontend3\public\images\3d-business-man-working-at-laptop-with-robot-assistant.png" 
+            alt="Business Man" 
+            class="image" 
+            style="width: 400px;" 
+          />
+        </div>
+      </v-row>
+    </v-container>
+  </template>
+  
+  <style>
+    .container {
+      height: 100vh; /* Full viewport height */
+    }
+    
+    .image {
+      max-width: 100%; /* Responsive image */
+    
+    }
   .rectangle {
-    width: 50%;
-    height: 2px;
-    position: relative;
-    margin-bottom: 20px;
-    box-sizing: border-box;
-    left: +18px
-}
-
-  .line {
-  width: 67%;  /* Full width of the rectangle */
-  height: 1px;
-  background-color: black;
-  position: absolute;
-  top: 50%;  /* Center the line vertically */
-  left: +30px;
- 
-}
-  .or-continue {
-    width: 33%;
-    background-color: white;
-    position: absolute;
-    top: -7px;
-    transform: translateX(-50%);
-    font-size: 9px;
-    color: black;
-    font-weight: bold;
-    left: 120px;
-    padding-left: 5px;
-    padding-right: 5px;
+      width: 50%;
+      height: 2px;
+      position: relative;
+      margin-bottom: 20px;
+      box-sizing: border-box;
+      left: +25px;
   }
-
-  .social-icons {
-    width: 25%;
-    display: flex;
-    padding: 1px;
-  }
-
-
-  .icon-link {
-    margin: 0 10px;
-  }
-  .icons{
-    width: 30px;
+  
+    .line {
+    width: 100%;  /* Full width of the rectangle */
     height: 30px;
+    height: 1px;
+    background-color: black;
+    position: absolute;
+    top: 50%;  /* Center the line vertically */
+    left: 40px;
+   
   }
-
-  .sign-up {
-    text-align: center;
-    background-color: #b6b2b2;
-    width: 100%;
-    color: #fff;
-    padding: 10px;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    margin-bottom: 20px;
-    left: -20px;
-  }
-
-  .sign-up-link {
-    color: #00B0FF;
-    text-decoration: none;
-  }
-</style>
+  
+  .or-continue {
+      width: 47%;
+      background-color: white;
+      position: absolute;
+      top: -7px; 
+      transform: translateX(-50%); 
+      font-size: 9px;
+      color: black;
+      font-weight: bold;
+      left: +80px;
+      padding-left: 5px;
+      padding-right: 1px;
+  
+      }
+    
+    .social-icons {
+      display: flex;
+      justify-content: center;
+      margin-bottom: 15px;
+      margin-left: -35px;
+  
+    }
+    
+    .icon-link {
+      margin: 0 10px;
+    }
+    .icons{
+      width: 30px;
+      height: 30px;
+    }
+    .sign-up {
+      text-align: center;
+      background-color: #b6b2b2;
+      width: 75%;
+      color: #fff;
+      padding: 10px;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+      margin-bottom: 20px;
+    }
+    
+    .sign-up-link {
+      color: #00B0FF;
+      text-decoration: none;
+    }
+    
+    
+  </style>
+  
+  
+  
--- components/auth/Signupped.vue
+++ components/auth/Signupped.vue
@@ -1,185 +1,120 @@
 <template>
-
-  <div class="forget-container">
-    
-  <div class="left-side">
-    <h2 class="welcome-text">Sign Up to Begin your Learning <br>journey</h2>
-    <input type="text" class="username-input" placeholder="Username" />
-    <input type="email" class="email-input" placeholder="Email" />
-    <input type="password" class="password-input" placeholder="Password" />
-    <div class="remember-forgot">
-          <a href="#" class="back-to-sign-in">Back in to Sign Up</a>
-    </div>
-    <button class="login-button">Send</button>
-    <div class="rectangle">
-        <div class="line">
-            <span class="or-continue">or continue with</span>
+    <v-container class="container fill-height mt-6" >
+      <v-row class="d-flex flex-row justify-center align-center">
+        <v-col class="d-flex flex-column justify-center align-center ml-16" style="align-items: center; margin-left: -20px;">
+          <h2 class="text-center" style="color: skyblue;">Sign Up to Begin Your Leaning <br> Journey</h2>
+  
+          <v-form class="d-flex flex-column mt-5 w-35" style="width: 350px; align-items: center;">
+            <v-text-field class="w-100" label="Username"></v-text-field>
+            <v-text-field class="w-100" label="Email"></v-text-field>
+            <v-text-field class="w-100" label="Password"></v-text-field>
+  
+            <v-col class="d-flex justify-end w-100">
+              <a href="#" style="margin-left: auto;">back to sign up</a>
+            </v-col>
+  
+            <v-container class="d-flex flex-column justify-center align-center" >
+              <div class="rectangle w-100">
+                <div class="line w-50">
+                  <span class="or-continue w-50">or continue with</span>
+                </div>
+              </div>
+              <div class="social-icons">
+                <a href="#" class="icon-link">
+                  <img src="C:\Owlmingo-Frontend3\public\icons\icons8-google-480.png" alt="Google" class="icons" />
+                </a>
+                <a href="#" class="icon-link">
+                  <img src="C:\Owlmingo-Frontend3\public\icons\icons8-github-480.png" alt="GitHub" class="icons" />
+                </a>
+                <a href="#" class="icon-link">
+                  <img src="C:\Owlmingo-Frontend3\public\icons\icons8-telegram-144.svg" alt="Telegram" class="icons" />
+                </a>
+              </div>
+  
+              <div class="sign-up" style="border: 1px solid black; width: 100%;">
+                <span>Don't have an account?</span>
+                <a href="#" class="sign-up-link">Sign Up</a>
+              </div>
+            </v-container>
+  
+          </v-form>
+        </v-col>
+  
+        <div class="right-side w-50 mr-n9">
+          <img 
+            src="C:\Owlmingo-Frontend3\public\images\3d-business-man-working-at-laptop-with-robot-assistant.png" 
+            alt="Business Man" 
+            class="image" 
+            style="width: 400px;" 
+          />
         </div>
-    </div>
-    <div class="social-icons">
-      <a href="#" class="icon-link">
-        <img src="hC:\Owlmingo-Frontend3\public\icons\icons8-google-480.png" alt="Google" class="icons" />
-      </a>
-      <a href="#" class="icon-link">
-        <img src="C:\Owlmingo-Frontend3\public\icons\icons8-github-480.png" alt="GitHub" class="icons"/>
-      </a>
-      <a href="#" class="icon-link">
-        <img src="hC:\Owlmingo-Frontend3\public\icons\icons8-telegram-144.svg" alt="Telegram" class="icons"/>
-      </a>
-    </div>
-    <div class="sign-up">
-      <span>Don't have an account?</span>
-      <a href="#" class="sign-up-link">Sign Up</a>
-    </div>
-  </div>
-
-  <div class="right-side" style="width: 50%; height: 100%;">
-      <img src="C:\Owlmingo-Frontend3\public\images\3d-business-man-working-at-laptop-with-robot-assistant.png" alt="" class="image" />
-      
-  </div>
-</div>
-</template> 
-
-<style scoped>
-.forget-container {
-  margin-left: 250px;
-  display: flex;
-  border-radius: 20px;
-  overflow: hidden;
-  justify-items: space-between;
-}
-
-.left-side {
-  background-color: #fff;
-  padding: 40px;
-  display: flex;
-  flex-direction: column;
-  justify-items: center;
-  align-items: center;
-}
-
-.right-side {
-  display: flex;
-  background-color: #ffffff;
-  justify-content: center;
-}
-
-.image{
-  width: 400px;
-  height: 570px;
-  margin-top: 35px;
-
-}
-
-.welcome-text {
-  width: 425px;
-  color: #00B0FF;
-  text-align-last : center;
-  font-size: 24px;
-  margin-bottom: 20px;
-
-}
-
-
-.email-input, .username-input, .password-input {
-  width: 75%;
-  padding: 10px;
-  margin-bottom: 20px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-}
-
-.remember-forgot {
-  display: flex;
-  font-size: 12px;
-  padding-bottom: 10px;
-  width: 75%;
-  justify-content: flex-end;
-}
-
-
-.back-to-sign-in {
-  color: #00B0FF;
-  text-decoration: none;
-
-}
-
-.login-button {
-  width: 75%;
-  background-color: #00B0FF;
-  color: #fff;
-  padding: 10px;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  margin-bottom: 20px;
-}
-
-.rectangle {
-  width: 50%;
-  height: 2px;
-  position: relative;
-  margin-bottom: 20px;
-  box-sizing: border-box;
-  left: +18px
-}
-
-.line {
-width: 90%;  /* Full width of the rectangle */
-height: 1px;
-background-color: black;
-position: absolute;
-top: 50%;  /* Center the line vertically */
-
-
-}
-
-.or-continue {
-  width: 40%;
-  background-color: white;
-  position: absolute;
-  top: -7px; 
-  transform: translateX(-50%); 
-  font-size: 9px;
-  color: black;
-  font-weight: bold;
-  left: +95px;
-  padding-left: 5px;
-  padding-right: 5px;
-
+      </v-row>
+    </v-container>
+  </template>
+  
+  <style>
+  .rectangle {
+    position: relative;
+    margin-bottom: 20px;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center; /* Center the rectangle horizontally */
   }
-
-.social-icons {
-  display: flex;
-  justify-content: center;
-  margin-bottom: 20px;
-  left: -20px
-
-}
-
-.icon-link {
-  margin: 0 10px;
-}
-.icons{
-  width: 30px;
-  height: 30px;
-}
-
-.sign-up {
-  text-align: center;
-  background-color: #b6b2b2;
-  width: 75%;
-  color: #fff;
-  padding: 10px;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  margin-bottom: 20px;
-}
-
-.sign-up-link {
-  color: #00B0FF;
-  text-decoration: none;
-}
-</style>
-
+  
+  .line { /* Full width of the rectangle */
+    background-color: black;
+    position: absolute;
+    top: 50%;  /* Center the line vertically */
+    display: flex;
+    justify-content: center; /* Center the text inside the line */
+  }
+  
+  .or-continue {
+    background-color: white;
+    position: absolute;
+    top: -7px; 
+    transform: translateX(-50%); 
+    font-size: 9px;
+    color: black;
+    font-weight: bold;
+    padding-left: 5px;
+    padding-right: 5px;
+  }
+    
+    .social-icons {
+      display: flex;
+      justify-content: center;
+      margin-bottom: 15px;
+      margin-left: -35px;
+  
+    }
+    
+    .icon-link {
+      margin: 0 10px;
+    }
+    .icons{
+      width: 30px;
+      height: 30px;
+    }
+    .sign-up {
+      text-align: center;
+      background-color: #b6b2b2;
+      width: 75%;
+      color: #fff;
+      padding: 10px;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+      margin-bottom: 20px;
+    }
+    
+    .sign-up-link {
+      color: #00B0FF;
+      text-decoration: none;
+    }
+    
+    
+  </style>
+  
+  
+  
