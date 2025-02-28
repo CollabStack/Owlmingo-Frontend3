9e5d564dd47fe650d022a4007fdeb74c5b78dfa9
Slade <ken.ex350@gmail.com>
2025-02-28 22:20:32 +0700

    change filename: SignUp.vue -> SignUp2.vue, SignIn.vue -> SIgnIn2.vue, Login.vue -> SignIn.vue, SignUpped.vue -> SignUp.vue

--- components/auth/SignUp.vue
+++ components/auth/SignUp.vue
@@ -1,38 +1,52 @@
-<template>
-    <v-container>
-        <v-row>
-        <v-col cols="12" md="6" offset-md="3">
-            <v-card>
-            <v-card-title>
-                <h3>Sign Up</h3>
-            </v-card-title>
-            <v-card-text>
-                <v-form>
-                <v-text-field
-                    v-model="email"
-                    label="Email"
-                    required
-                ></v-text-field>
-                <v-text-field
-                    v-model="password"
-                    label="Password"
-                    required
-                    type="password"
-                ></v-text-field>
-                <v-btn
-                    color="#4667E5"
-                    @click="signUp"
-                >
-                    Sign Up
-                </v-btn>
-                </v-form>
-            </v-card-text>
-            </v-card>
-        </v-col>
-        </v-row>
-    </v-container>
-</template>
-
-<script setup> 
+<script setup lang="ts">
+import Socialicon from './Socialicon.vue';
 
 </script>
+
+<template>
+    <v-container class="container fill-height" >
+      <v-row class="d-flex flex-row justify-center align-center">
+        <v-col class="d-flex flex-column align-center" style="height: 780px;">
+          <p  style= "color: var(--v-royal_blue);  width: 569px; text-align: center; font-size: 40px;">Sign Up to Begin Your Leaning Journey</p>
+
+          <v-form class="d-flex flex-column mt-5 " style="width: 504px; height: 350px; align-items: center; ">
+            <v-text-field
+              class="w-100 pb-0"
+              style= "height: 65px;"
+              label="Username" variant="outlined"
+            ></v-text-field>
+            <v-text-field
+              class="w-100 pb-0"
+              style= "height: 65px;"
+              label="Email" variant="outlined"
+            ></v-text-field>
+            <v-text-field
+              class="w-100 pb-0"
+              style= "height: 65px;"
+              label="Password" variant="outlined"
+            ></v-text-field>
+  
+            <v-col class="d-flex justify-end w-100 " style="top: -20px; width: 100%;">
+              <a href="#" style="margin-left: auto; font-size: 16px; text-decoration: none;">Back to Sign</a>
+            </v-col>
+  
+            <v-btn class="justify-center align-center " style="width: 504px; height: 73px; background-color: var(--v-maya_blue); border-radius: 10px; color: white; font-size: 24px;">Send</v-btn>
+
+            <Socialicon />
+  
+          </v-form>
+        </v-col>
+  
+        <div class="right-side w-50 mr-n9">
+          <img 
+            src= "/images/3d-business-man-working-at-laptop-with-robot-assistant.png" 
+            alt="Business Man" 
+            class="image" 
+            style="width: 602px; height: 828px;" 
+          />
+        </div>
+      </v-row>
+    </v-container>
+  </template>
+  
+  
