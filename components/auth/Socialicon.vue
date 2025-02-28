bf42a94005d8c55a3e1bfd7106f69cf18d946d70
Slade <ken.ex350@gmail.com>
2025-02-28 22:05:11 +0700

    combine script and styles into one file Socialicon.vue, resize otp input, and customize footer

---
+++ components/auth/Socialicon.vue
@@ -0,0 +1,77 @@
+<template>
+        <v-container class="d-flex flex-column justify-center align-center">
+              <v-row class="d-flex flex-row w-100 align-center pt-3">
+                  <v-divider class="border-opacity-100" style="flex: 2; color: var(--v-sonic_silver);"></v-divider>
+                  <span class=" pl-1 pr-1" style="font-size: 16px; color: var(--v-sonic_silver);">Or continue with</span>
+                  <v-divider class="border-opacity-100" style="flex: 2; color: var(--v-sonic_silver);"></v-divider>
+                </v-row>
+              
+              <v-row class="mt-6 justify-center justify-space-between" style = "width: 198px; height: 92px;">
+              <a
+                v-for="(icon, index) in socialIcons"
+                :key="index"
+                :href="icon.link"
+              >
+                <img :src="`/icons/${icon.src}`" :alt="icon.alt" style="width: 52px; height: 52px; padding: auto;"/>
+              </a>
+            </v-row>
+  
+            <div class="sign-up">
+              <span style="color: var(--v-sonic_silver); font-size: 20px;">
+                    Don't have an account ? 
+                <a href="#" class="sign-up-link">Sign Up</a>
+              </span>
+            </div>
+            </v-container>
+
+</template>
+
+<style>
+    
+
+    .sign-up {
+      align-content: center;
+      text-align: center;
+      background-color: var(--v-hex);
+      border-radius: 10px;
+      cursor: pointer;
+      background-color: var(--v-mercury);
+      width: 411px; 
+      height: 67px;
+    }
+    
+    .sign-up-link {
+      color: var(--v-maya_blue);
+      text-decoration: none;
+    }
+    
+    
+  </style>
+  
+  
+  <script>
+export default {
+  data() {
+    return {
+      socialIcons: [
+        {
+          src: 'icons8-google-480.png', // Just the filename
+          alt: 'Google',
+          link: '#',
+        },
+        {
+          src: 'icons8-github-480.png', // Just the filename
+          alt: 'GitHub',
+          link: '#',
+        },
+        {
+          src: 'icons8-telegram-144.svg', // Just the filename
+          alt: 'Telegram',
+          link: '#',
+        },
+      ],
+    };
+  },
+};
+</script>
+  
