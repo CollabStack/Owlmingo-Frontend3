b0b7418bbc9baeeaa19fed4d77b11b9b55e13198
Soksan369 <147368894+Soksan369@users.noreply.github.com>
2025-03-05 01:07:47 +0700

    Update Login Signup Page

--- pages/auth/index.vue
+++ pages/auth/index.vue
@@ -1,8 +1,9 @@
 <template>
-    <Auth-SignIn />
+    <SignIn />
+    <!-- <Auth-SignIn /> -->
     <!-- <TelegramAuth /> -->
-
+   <Footer></Footer>
 </template>
 <script setup>
-
+import SignIn from '~/components/auth/SignIn2.vue';
 </script>
