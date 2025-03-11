5469eb522d595c886e11ba9ec465fee7850bfc38
Soksan369 <147368894+Soksan369@users.noreply.github.com>
2025-03-11 22:41:50 +0700

    otp-sign-up-login-and-reset-password

---
+++ pages/auth/forgot-password.vue
@@ -0,0 +1,12 @@
+<template>
+    <section>
+        <ForgetPassword />
+    </section>
+</template>
+<script setup>
+import ForgetPassword from '~/components/auth/ForgetPassword.vue';
+
+</script>
+<style scoped>
+
+</style>
