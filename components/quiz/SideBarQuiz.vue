75c1a0b34bd8fdb9ff92f2bc5d9d66f760f6b6a0
SocheatKhoeun <159861545+SocheatKhoeun@users.noreply.github.com>
2025-03-15 07:36:09 +0700

    Complete Quiz Implement

--- components/quiz/SideBarQuiz.vue
+++ components/quiz/SideBarQuiz.vue
@@ -11,6 +11,7 @@
             density="compact" 
             hide-details 
             class="mt-2 rounded-lg bg-blue-lighten-5"
+            placeholder="Enter name"
         />
 
         <!-- Checkbox Options -->
@@ -37,26 +38,34 @@
             </v-col>
         </v-row>
          
-        <div class="container">
+        <!-- <div class="container">
             <v-spacer></v-spacer>
-            <v-btn variant="outlined" color="blue" class="w-100 mb-3 bottom-button rounded-lg">
+            <v-btn variant="outlined" color="blue" class="w-100 mb-3 bottom-button rounded-lg" @click = "resultsQuiz">
                 Completions & Responses
             </v-btn>
-        </div>
+        </div> -->
     </v-navigation-drawer>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+// import { useRouter } from 'vue-router'; // Import useRouter
+  
+// const router = useRouter(); 
 
 // Reactive State
-const name = ref('Lesson Name');
+const name = ref('');
 
 // Checkboxes reactive
 const options = ref([
     { model: ref(true), label: "Show Answers", tooltip: "Toggle to show correct answers after submission." },
     { model: ref(false), label: "Shuffle Questions", tooltip: "Randomizes the order of questions in the quiz." }
 ]);
+
+// Method that show Results Quiz
+// const resultsQuiz = () => {
+//     router.push('/quiz/results-quiz');
+// };
 </script>
 
 <style scoped>  
---
+++ pages/quiz/test.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-container class="d-flex justify-center">
+      <v-card class="pa-5 rounded-xl" max-width="800">
+        <v-card-title class="text-h5 d-flex justify-space-between align-center w-100">
+          Answers
+          <!-- Close button placed to the right -->
+          <v-btn @click="response" class="ml-auto text-blue" variant="flat">
+            Close
+          </v-btn>
+        </v-card-title>
+        <v-card-subtitle class="text-body-1">Score: 1/5</v-card-subtitle>
+      
+        <v-divider class=""></v-divider>
+        <ResultsForm />
+      </v-card>
+    </v-container>
+  </template>
+  
+  <script setup>
+  import { useRouter } from 'vue-router'; // Import useRouter
+  import ResultsForm from '~/components/quiz/ResultsForm.vue';
+  
+  const router = useRouter(); // Initialize router
+  
+  const response = () => {
+    router.push('/quiz/complete-quiz'); // Navigate to the desired route
+  };
+  </script>
+  
+  <style scoped>
+  </style>
+  
