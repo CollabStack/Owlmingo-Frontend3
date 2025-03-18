74346a73220c688054b297cd95a093e0df1e8719
Uch-Mengly <mengly_uch@student.tuxglobalinstitute.edu.kh>
2025-03-18 22:27:12 +0700

    Remove merge conflict markers from library.vue

--- pages/flashcard/library.vue
+++ pages/flashcard/library.vue
@@ -3,20 +3,3 @@
         <Flashcards-Library />
     </div>
 </template>
-
-<script setup>
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-</script>
-=======
-</script>
->>>>>>> main
-=======
-
-
-</script>
-
-</script>
-
->>>>>>> d43d874b7b8149b3f0e3125ce75094c3a23249c8
